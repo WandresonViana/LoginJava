a299b2d2ee0ce9a36239bbf69a56b93bd39cf37e
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-14 15:24:00 -0300

    Iniciando tela de login

---
+++ src/main/java/com/mycompany/login01/Login01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login01;
+
+/**
+ *
+ * @author wandreson
+ */
+public class Login01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
